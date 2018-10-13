f82ad3c3ca5eaeeb5089fb1591fb6b6b3937cb46
steve-bakos <alithvia@gmail.com>
2018-10-13 09:21:41 -0400

    testing

--- action-card-problem.py
+++ action-card-problem.py
@@ -10,14 +10,12 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 INTENT_ENTRY_CREDIT_CARD_PROBLEM = "AIHub:EntryCreditCardProblem"
-# INTENT_ANSWER = "give_answer"
+INTENT_CREDIT_CARD_PROBLEM_STAGE_2 = "AIHub:CreditCardProblemStage2"
 # INTENT_INTERRUPT = "interrupt"
 # INTENT_DOES_NOT_KNOW = "does_not_know"
 
-INTENT_FILTER_GET_ANSWER = [
-    INTENT_ANSWER,
-    INTENT_INTERRUPT,
-    INTENT_DOES_NOT_KNOW
+INTENT_FILTER_GET_RESPONSE = [
+    INTENT_CREDIT_CARD_PROBLEM_STAGE_2
 ]
 
 SessionsStates = {}
@@ -27,11 +25,13 @@ def user_starts_entry_card_problem(hermes, intent_message):
 
     sentence = "Try again with the black stripe facing the ground on the right."
 
-    # session_state, sentence = tt.start_quiz(number_of_questions, tables)
+    hermes.publish_continue_session(intent_message.session_id, sentence, INTENT_FILTER_GET_RESPONSE)
 
-    # tt.save_session_state(SessionsStates, intent_message.session_id, session_state)
+def user_starts_entry_card_problem_stage_2(hermes, intent_message):
+    print("User has a card problem stage 2.")
 
-    hermes.publish_continue_session(intent_message.session_id, sentence, INTENT_FILTER_GET_ANSWER)
+    sentence = "Check that the back of your card is clean."
+    hermes.publish_end_session(intent_message.session_id, sentence)
 
 def session_started(hermes, session_started_message):
     print("Session Started")
@@ -53,16 +53,17 @@ def session_ended(hermes, session_ended_message):
     session_id = session_ended_message.session_id
     session_site_id = session_ended_message.site_id
 
-    # if SessionsStates.get(session_id) is not None:
-    #     hermes.publish_start_session_action(site_id=session_site_id,
-    #                                         session_init_text="",
-    #                                         session_init_intent_filter=INTENT_FILTER_GET_ANSWER,
-    #                                         session_init_can_be_enqueued=False,
-    #                                         custom_data=session_id)
+    if SessionsStates.get(session_id) is not None:
+        hermes.publish_start_session_action(site_id=session_site_id,
+                                            session_init_text="",
+                                            session_init_intent_filter=INTENT_FILTER_GET_RESPONSE,
+                                            session_init_can_be_enqueued=False,
+                                            custom_data=session_id)
 
 with Hermes(MQTT_ADDR) as h:
 
     h.subscribe_intent(INTENT_ENTRY_CREDIT_CARD_PROBLEM, user_starts_entry_card_problem) \
+        .subscribe_intent(INTENT_CREDIT_CARD_PROBLEM_STAGE_2, user_starts_entry_card_problem_stage_2) \
         .subscribe_session_ended(session_ended) \
         .subscribe_session_started(session_started) \
         .start()
